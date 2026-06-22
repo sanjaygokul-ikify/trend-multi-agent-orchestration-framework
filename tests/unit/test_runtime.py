c8d432f7c2f39f930e39de4b13f25b83963292b0
Sanjay G <gokulesh2615@gmail.com>
2026-06-22 17:20:11 +0530

    test(unit): add unit test suite with assertions on core invariants

---
+++ tests/unit/test_runtime.py
@@ -0,0 +1,11 @@
+import unittest
+from packages.core import AgentRuntime
+
+
+class TestRuntime(unittest.TestCase):
+    def test_agent_runtime(self) -> None:
+        agent_runtime = AgentRuntime()
+        # Add assertions
+
+if __name__ == '__main__':
+    unittest.main()
