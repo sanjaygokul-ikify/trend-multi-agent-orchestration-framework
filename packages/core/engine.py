64fed92406298d67af358c0cba0885a4beb13866
Sanjay G <gokulesh2615@gmail.com>
2026-06-22 17:19:53 +0530

    feat(engine): implement primary domain engine

---
+++ packages/core/engine.py
@@ -0,0 +1,65 @@
+import logging
+from typing import List, Dict
+from .types import AgentRuntime, TaskScheduler, WorkerPod, EventBus, PersistentStore, AnalyticsDB
+from .exceptions import CoreException, EngineException
+
+class Engine:
+    def __init__(self, agent_runtime: AgentRuntime, task_scheduler: TaskScheduler, worker_pod: WorkerPod, event_bus: EventBus, persistent_store: PersistentStore, analytics_db: AnalyticsDB):
+        self.agent_runtime = agent_runtime
+        self.task_scheduler = task_scheduler
+        self.worker_pod = worker_pod
+        self.event_bus = event_bus
+        self.persistent_store = persistent_store
+        self.analytics_db = analytics_db
+        self.logger = logging.getLogger(__name__)
+
+    def start(self) -> None:
+        self.logger.info('Starting engine')
+        self.task_scheduler.start()
+        self.worker_pod.start()
+        self.event_bus.start()
+        self.persistent_store.start()
+        self.analytics_db.start()
+
+    def stop(self) -> None:
+        self.logger.info('Stopping engine')
+        self.task_scheduler.stop()
+        self.worker_pod.stop()
+        self.event_bus.stop()
+        self.persistent_store.stop()
+        self.analytics_db.stop()
+
+    def execute(self, task: Dict) -> None:
+        self.logger.info('Executing task')
+        self.task_scheduler.schedule(task)
+        self.worker_pod.allocate(task)
+        self.event_bus.publish(task)
+        self.persistent_store.store(task)
+        self.analytics_db.query(task)
+
+    def orchestrate(self, tasks: List[Dict]) -> None:
+        self.logger.info('Orchestrating tasks')
+        for task in tasks:
+            self.execute(task)
+
+class CoreException(Exception):
+    pass
+
+class EngineException(CoreException):
+    pass
+
+try:
+    engine = Engine(
+        AgentRuntime(),
+        TaskScheduler(),
+        WorkerPod(),
+        EventBus(),
+        PersistentStore(),
+        AnalyticsDB()
+    )
+    engine.start()
+    engine.orchestrate([{'task_id': 1, 'task_name': 'task_1'}, {'task_id': 2, 'task_name': 'task_2'}])
+except EngineException as e:
+    logging.error(f'Engine exception: {e}')
+except CoreException as e:
+    logging.error(f'Core exception: {e}')
